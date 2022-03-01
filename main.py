d657b0a9fb2c9a1fe8276d848aa03a2abe0f2991
YilingChen <shutuzi88@gmail.com>
2022-03-01 14:37:22 +0800

    Optimize Code

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 # -*- coding:utf-8 -*-
 
-# import nltk
-# nltk.download('words')
 from nltk.corpus import words
-# dynamic list
 
-words_ = [K.lower() for K in words.words() if len(K) == 5]
-reserved = []
-unreserved = []
+Words_ = [K.lower() for K in words.words() if len(K) == 5]
+Reserved = []
+Unreserved = []
+
 def feedback(times=None):
-    global words_, reserved, unreserved
+    global Words_, Reserved, Unreserved
     while times < 7:
         times += 1
         print("You left {} time(s) guessing !".format(7 - times))
@@ -20,25 +18,22 @@ def feedback(times=None):
             break
         else:
             for i, c in enumerate(word):
-                [words_.remove(word) for k in words_ if word in words_]
+                [Words_.remove(word) for k in Words_ if word in Words_]
                 if c.lower() == feedback[i].lower():
-                    words_ = [K for K in words_ if c in K]
                     if feedback[i].isupper():
-                        reserved.append(feedback[i].lower())
-                        print("28 c:", c)
-                        words_ = [K for K in words_ if K[i] == c]
+                        Reserved.append(feedback[i].lower())
+                        Words_ = [K for K in Words_ if K[i] == c]
                     elif feedback[i].islower():
-                        print("30: ", feedback[i])
-                        reserved.append(feedback[i])
-                        print("32 reserved: ", reserved)
-                        words_ = [K for K in words_ if c in K and K[i] != c]
+                        Reserved.append(feedback[i])
+                        print("32 Reserved: ", Reserved)
+                        Words_ = [K for K in Words_ if c in K and K[i] != c]
                 else:
                     if feedback[i] == "-":
-                        unreserved.append(c)
-                        intersection = list(set(unreserved) & set(reserved))
-                        print("40 intersection: ", intersection)
-                        print("41 unreserved: ", unreserved)
-                        words_ = [K for K in words_ if c in reserved or c not in K]
+                        Unreserved.append(c)
+                        # intersection = list(set(Unreserved) & set(Reserved))
+                        # print("40 intersection: ", intersection)
+                        print("41 Unreserved: ", Unreserved)
+                        Words_ = [K for K in Words_ if c in Reserved or c not in K]
                         continue
-            print(words_)
+            print(Words_)
 feedback(0)
