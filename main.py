796aad7f2708b50a5a1f26eeea5aaeb35d26dcdb
YilingChen <shutuzi88@gmail.com>
2022-03-11 11:39:15 +0800

    add intersection

--- main.py
+++ main.py
@@ -9,17 +9,17 @@
 
 from nltk.corpus import words
 
-Words_ = [K.lower() for K in words.words() if len(K) == 5]
-Reserved = []
-Unreserved = []
 
 def feedback(times=None):
-    global Words_, Reserved, Unreserved
+    Words_ = [K.lower() for K in words.words() if len(K) == 5]
     while times < 7:
         times += 1
         print("You left {} time(s) guessing !".format(7 - times))
         word = input("please type the word: ")
         feedback = input("please type the result: ")
+        word_ = list(set([k for k in word.lower()]))
+        feedback_ = list(set([k for k in feedback.lower()]))
+        intersection = list(set(word_) & set(feedback_))
         if word.upper() == feedback.upper():
             print("Good job, see you tomorrow.")
             break
@@ -28,18 +28,11 @@ def feedback(times=None):
                 [Words_.remove(word) for k in Words_ if word in Words_]
                 if c.lower() == feedback[i].lower():
                     if feedback[i].isupper():
-                        Reserved.append(feedback[i].lower())
                         Words_ = [K for K in Words_ if K[i] == c]
                     elif feedback[i].islower():
-                        Reserved.append(feedback[i])
-                        print("32 Reserved: ", Reserved)
                         Words_ = [K for K in Words_ if c in K and K[i] != c]
                 else:
                     if feedback[i] == "-":
-                        Unreserved.append(c)
-                        # intersection = list(set(Unreserved) & set(Reserved))
-                        # print("40 intersection: ", intersection)
-                        print("41 Unreserved: ", Unreserved)
-                        Words_ = [K for K in Words_ if c in Reserved or c not in K]
+                        Words_ = [K for K in Words_ if c in intersection or c not in K]
             print(Words_)
 feedback(0)
